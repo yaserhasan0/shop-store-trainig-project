0c36c5409c6a131a2a98947c4a78cdc1b2dc80a5
yaserhasan0 <ing.yaserhasan@gmail.com>
2024-12-28 16:02:47 +0200

    Products Slider

--- js/script.js
+++ js/script.js
@@ -1,9 +1,51 @@
 // init Isotope
 var $products = $('.all-products').isotope({
     // options
+    originLeft: false,
   });
   // filter items on button click
   $('.filter-button-group').on( 'click', 'button', function() {
     var filterValue = $(this).attr('data-filter');
     $products.isotope({ filter: filterValue });
+  });
+
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 6,
+        spaceBetween: 15,
+      },
+      // 1024: {
+      //   slidesPerView: 5,
+      //   spaceBetween: 50,
+      // },
+    },
   });
